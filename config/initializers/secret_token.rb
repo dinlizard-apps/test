28ead5722065c6f83e02b1e30e4258e30faae30e
cddin <din@lizard-apps.com>
2014-07-31 15:02:59 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyOnRails01::Application.config.secret_key_base = '6c53d2e43c6af887696618e574463ea3d73e9294f90e6dfcfe7401d95cc1f9e5af32393778d2bd78edd82ab04e38b819e9282c666cae9f13d06b8b43fa7da3ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyOnRails01::Application.config.session_store :cookie_store, key: '_rubyOnRails01_session'
